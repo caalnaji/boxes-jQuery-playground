46dfed0559cbee48fd14c5bebb3853995450bc3d
Chloe Alnaji <caedeal@gmail.com>
2015-10-15 18:40:01 -0600

    Completed all tasks in playground

--- boxes.js
+++ boxes.js
@@ -1,1 +1,31 @@
-console.log("hello world");
+$(document).ready(function() {
+  console.log("hello world");
+  $("#secretBox").css("background", "white").html("<h1>secret box!</h1>");
+  $("#row1").children().removeClass().addClass("box boxType3");
+  $("#container div:last-child div:last-child").remove();
+  $(".boxType1").css("background", "white");
+  $("#row2 div:first-child, #row2 div:nth-child(2)").removeClass();
+  // $("#container").find("div:not(.row, #secretBox)").css("width", "10px");
+  $("#container").click(function(evt) {
+    console.log("x: ", evt.pageX, " y: ", evt.pageY);
+  });
+  $(".boxType1").html("<a href='http://students.galvanize.com'>link</a>");
+  $("a").click(function(evt) {
+    alert("You can never leave!!!!");
+    evt.preventDefault();
+  });
+  $(".box").one("click", function(evt) {
+    $(this).html("<img src='http://spenglercounseling.com/wp-content/uploads/2013/01/Cute-puppy.jpg'>");
+    $(".box").click(function() {
+      $("img", this).toggle();    
+    });
+  });
+  $("#container").click(function(evt) {
+    if (evt.target !== this) {
+      $(this).css("background", "black");
+      $(evt.target).css("background", "white");
+    } else {
+      $(this).css("background", "green");
+    }
+  });
+});
